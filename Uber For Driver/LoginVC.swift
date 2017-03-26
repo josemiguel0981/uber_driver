a79b7fea886586ec2f3ef736f4151843664ce4b2
Haider Rasool <hyder.lyger@gmail.com>
2017-03-26 12:36:10 -0700

    Starting the project

---
+++ Uber For Driver/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVC.swift
+//  Uber For Driver
+//
+//  Created by Haider Rasool on 3/26/17.
+//  Copyright © 2017 Haider Rasool. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var tb_email: UITextField!
+    @IBOutlet weak var tb_password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func action_login(_ sender: Any) {
+    }
+
+    @IBAction func action_signup(_ sender: Any) {
+    }
+}
